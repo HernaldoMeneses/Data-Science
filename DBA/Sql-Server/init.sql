da6c69239f8df85f43e136c62aa936d41bb6cf73
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-10-05 17:22:16 -0300

    Update init.sql

--- DBA/Sql-Server/init.sql
+++ DBA/Sql-Server/init.sql
@@ -1,2 +1,5 @@
 --Welcome to DataBase Administraror by ./HernaldoMeneses
 -- objetivo -> aprender T-SQL (Transact-SQL)
+-- Getting Started
+-- T-SQL - Transact-SQL is Microsoft's implemntation 
+-- of the Structured Query Language (SQL)
