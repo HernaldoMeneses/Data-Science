78a182f7912860532cd96a801d7e0bc965e062bc
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-10-05 17:44:22 -0300

    Update init.sql

--- DBA/Sql-Server/init.sql
+++ DBA/Sql-Server/init.sql
@@ -3,3 +3,10 @@
 -- Getting Started
 -- T-SQL - Transact-SQL is Microsoft's implemntation 
 -- of the Structured Query Language (SQL)
+
+-- Install the SQL Server... 
+--- continue__
+
+-- Using SQL Server Management Studio
+
+SELECT * FROM sys.databases;
