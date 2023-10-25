c4d280988830571265154e9418121fcbde97fd75
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-10-25 12:19:27 -0300

    .//

---
+++ Adm-Sys/SQLPlus.cmd
@@ -0,0 +1,35 @@
+@echo off
+cls
+echo - ########### - Guia Inicial.                                                           ---#
+echo - #                                                                                       -#
+echo - # - Objetivo: Help to conetct and colsult Oracle Data base by Prompt
+echo - # - Obs     : Using.
+echo - #
+echo - # - Titulo  : Oracle_prompt_sql.
+echo - # - Tema    : conect and consult.
+echo - #
+echo - # - Autor   : Hernaldo Meneses 
+echo - # - Criação : 25/10/2023
+echo - # - more info and of Script.
+echo - #----------------------------------------------------------------------------------Thanks.
+echo ---
+echo --
+echo -
+echo --------------------------------------------------------------------------------------------
+echo *   Ben vindo ao assistente de conslta Sql Oracle via prompt by Hm_                        * 
+echo ____________________________________________________________________________________________
+echo -
+echo - Vefique se sqlplus (Oracle)  está instalado digitando: sqlplus -v
+echo -
+echo - Para Conectar ao banco digite o comando e as credenciais 
+echo - conforme formato a seguir
+echo - CONNECT user/passwordd@name_db
+echo -
+echo - Agora Realize Consultas do tipo
+echo - SELECT count(distinct(table_name)) From User_tab_Columns;
+echo -
+echo -
+echo - as consultas via sqlplus retornao tabelas #linhas e colunas 'soltas'
+echo - é aconselhavel utilizar o packg i_ python desenvolvido pela oracle 
+echo - para facilitar a tranformação das tabelas # em objetos manipuláveis convencionais
+echo ____________________________________________________________________________________________
