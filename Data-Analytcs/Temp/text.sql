7c59cce3c22bbd3fa186977230882301971a2359
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-11-08 18:34:04 -0300

    /M

---
+++ Data-Analytcs/Temp/text.sql
@@ -0,0 +1,194 @@
+PositCLiNat:=COUNTROWS(SUMMARIZE(Rel_NatOne;[CODCLI]))
+%_Posit:=[PositCLiNat]/[Qt_Cli]
+Ganho R$:=sum(Rel_NatOne[50])
+
+SELECT
+    PCPEDC.CODGERENTE  
+    , PCPEDC.CODSUPERVISOR CODSUPERVISOR
+--    , PCPEDC.POSICAO
+--    , PCPEDC.DATA
+    , PCPEDC.CODUSUR
+--    , PCPEDC.CODFILIAL
+--    , PCPRODUT.CODEPTO
+    , PCFORNEC.codfornec
+--    , PCPRODUT.CODPROD
+    , PCPEDC.CODCLI,
+       SUM(CASE                                                                                                                            
+             WHEN NVL(PCPEDI.BONIFIC, 'N') = 'N' THEN                                                                                  
+              DECODE(PCPEDC.CONDVENDA,                                                                                                     
+                     5,                                                                                                                    
+                    0,
+                     6,                                                                                                                    
+                     0,                                                                                                                    
+                     11,                                                                                                                   
+                     0,                                                                                                                    
+                     12,                                                                                                                   
+                     0,                                                                                                                    
+                     NVL(PCPEDI.VLSUBTOTITEM,                                                                                              
+                         (DECODE(NVL(PCPEDI.TRUNCARITEM, 'N'),                                                                           
+                                 'N',                                                                                                    
+                                 ROUND((NVL(PCPEDI.QT, 0)) * (NVL(PCPEDI.PVENDA, 0) + nvl(pcpedi.vloutrasdesp,0) + 
+nvl(pcpedi.vlfrete,0)), 
+                                       2),                                                                                                 
+                                 TRUNC((NVL(PCPEDI.QT, 0)) * (NVL(PCPEDI.PVENDA, 0) + nvl(pcpedi.vloutrasdesp,0) + 
+nvl(pcpedi.vlfrete,0)), 
+                                       2)))))                                                                                              
+             ELSE                                                                                                                          
+    0 
+           END) - SUM(CASE                                                                                                                 
+                        WHEN NVL(PCPEDI.BONIFIC, 'N') = 'N' THEN                                                                       
+                         DECODE(PCPEDC.CONDVENDA,                                                                                          
+                                5,                                                                                                         
+                                0,                                                                                                         
+                                6,                                                                                                         
+                                0,                                                                                                         
+                                11,                                                                                                        
+                                0,                                                                                                         
+                                12,                                                                                                        
+                                0,                                                                                                         
+                                NVL(PCPEDI.qt, 0) * (0 + 0))                                                                               
+                        ELSE                                                                                                               
+                    0
+                      END) VLVENDA,     
+
+
+    Case When (
+
+
+               SUM(CASE                                                                                                                            
+             WHEN NVL(PCPEDI.BONIFIC, 'N') = 'N' THEN                                                                                  
+              DECODE(PCPEDC.CONDVENDA,                                                                                                     
+                     5,                                                                                                                    
+                    0,
+                     6,                                                                                                                    
+                     0,                                                                                                                    
+                     11,                                                                                                                   
+                     0,                                                                                                                    
+                     12,                                                                                                                   
+                     0,                                                                                                                    
+                     NVL(PCPEDI.VLSUBTOTITEM,                                                                                              
+                         (DECODE(NVL(PCPEDI.TRUNCARITEM, 'N'),                                                                           
+                                 'N',                                                                                                    
+                                 ROUND((NVL(PCPEDI.QT, 0)) * (NVL(PCPEDI.PVENDA, 0) + nvl(pcpedi.vloutrasdesp,0) + 
+nvl(pcpedi.vlfrete,0)), 
+                                       2),                                                                                                 
+                                 TRUNC((NVL(PCPEDI.QT, 0)) * (NVL(PCPEDI.PVENDA, 0) + nvl(pcpedi.vloutrasdesp,0) + 
+nvl(pcpedi.vlfrete,0)), 
+                                       2)))))                                                                                              
+             ELSE                                                                                                                          
+    0 
+           END) - SUM(CASE                                                                                                                 
+                        WHEN NVL(PCPEDI.BONIFIC, 'N') = 'N' THEN                                                                       
+                         DECODE(PCPEDC.CONDVENDA,                                                                                          
+                                5,                                                                                                         
+                                0,                                                                                                         
+                                6,                                                                                                         
+                                0,                                                                                                         
+                                11,                                                                                                        
+                                0,                                                                                                         
+                                12,                                                                                                        
+                                0,                                                                                                         
+                                NVL(PCPEDI.qt, 0) * (0 + 0))                                                                               
+                        ELSE                                                                                                               
+                    0
+                      END)  
+
+    ) >= 50 then 5 else 0 end "50++",
+
+
+
+
+
+
+
+   --SUM((PCPEDI.qt)*NVL(PCPEDI.vlcustofin,0)) AS VLCUSTOFIN,
+   SUM(DECODE(PCPEDC.CONDVENDA,1,CASE WHEN NVL(PCPEDI.BONIFIC, 'N') <> 'N' THEN 0 ELSE NVL(PCPEDI.QT, 0) END,5,0,6,0,11,0,12,0,
+NVL(PCPEDI.QT,0))) QT,
+/****
+    SUM(DECODE(PCPEDC.CONDVENDA,1,CASE WHEN NVL(PCPEDI.BONIFIC, 'N') IN ('S','F') THEN NVL(PCPEDI.QT, 0) ELSE 0 END,5,NVL(PCPEDI.QT,
+        0),6,NVL(PCPEDI.QT, 0),11,NVL(PCPEDI.QT, 0),12,NVL(PCPEDI.QT, 0),0)) QTBNF,
+    ***/
+COUNT(DISTINCT PCCLIENT.CODCLI) AS QTCLIENTE,
+--SUM( NVL(PCPEDI.QT,0) * NVL(PCPRODUT.PESOBRUTO,0) ) AS PESO,
+COUNT(DISTINCT(PCPEDC.NUMPED)) AS QTPED 
+/****
+       SUM(DECODE(PCPEDC.CONDVENDA,                                                                                                        
+                  5,                                                                                                                       
+                  NVL(PCPEDI.QT, 0) *                                                                                                      
+                  DECODE(NVL(PCPEDI.PBONIFIC, 0),                                                                                          
+                         0,                                                                                                                
+                         PCPEDI.PTABELA,                                                                                                   
+                         PCPEDI.PBONIFIC),                                                                                                 
+                  6,                                                                                                                       
+                  NVL(PCPEDI.QT, 0) *                                                                                                      
+                  DECODE(NVL(PCPEDI.PBONIFIC, 0),                                                                                          
+                         0,                                                                                                                
+                         PCPEDI.PTABELA,                                                                                                   
+                         PCPEDI.PBONIFIC),                                                                                                 
+                  1,                                                                                                                       
+                  CASE                                                                                                                     
+                    WHEN NVL(PCPEDI.BONIFIC, 'N') <> 'N' THEN                                                                          
+                     NVL(PCPEDI.QT, 0) *                                                                                                   
+                     DECODE(NVL(PCPEDI.PBONIFIC, 0), 0, PCPEDI.PTABELA, PCPEDI.PBONIFIC)                                                   
+                    ELSE                                                                                                                   
+                     0                                                                                                                     
+                  END,                                                                                                                     
+                  11,                                                                                                                      
+                  NVL(PCPEDI.QT, 0) *                                                                                                      
+                  DECODE(NVL(PCPEDI.PBONIFIC, 0),                                                                                          
+                         0,                                                                                                                
+                         PCPEDI.PTABELA,                                                                                                   
+                         PCPEDI.PBONIFIC),                                                                                                 
+                  12,                                                                                                                      
+                  NVL(PCPEDI.QT, 0) *                                                                                                      
+                  DECODE(NVL(PBONIFIC, 0),                                                                                                 
+                         0,                                                                                                                
+                         PCPEDI.PTABELA,                                                                                                   
+                         PCPEDI.PBONIFIC),                                                                                                 
+                  0)) VLBONIFIC  
+        ***/                                                                                                          
+FROM 
+PCPEDI
+, PCPEDC
+, PCSUPERV
+, PCPRODUT
+, PCDEPTO
+, PCCLIENT
+, PCUSUARI
+, PCPRACA
+, PCFORNEC
+
+WHERE 1=1
+AND PCPEDC.DATA >= to_date('01/11/2023','dd/mm/yyyy') AND PCPEDC.DATA <= to_date('30/11/2023','dd/mm/yyyy')
+AND PCPEDI.DATA >= to_date('01/11/2023','dd/mm/yyyy') AND PCPEDI.DATA <= to_date('30/11/2023','dd/mm/yyyy')
+
+
+--AND TO_CHAR(PCPEDC.DATA , 'YYYY') >= TO_CHAR(ADD_MONTHS(SYSDATE, -24), 'YYYY')
+--AND TO_CHAR(PCPEDI.DATA , 'YYYY') >= TO_CHAR(ADD_MONTHS(SYSDATE, -24), 'YYYY')
+
+AND PCPEDI.NUMPED = PCPEDC.NUMPED
+AND  PCPEDC.CODSUPERVISOR  = PCSUPERV.CODSUPERVISOR
+AND PCPEDI.CODPROD = PCPRODUT.CODPROD
+AND PCPRODUT.CODEPTO = PCDEPTO.CODEPTO
+AND  PCPEDC.CODUSUR  = PCUSUARI.CODUSUR
+AND PCPEDC.CODCLI = PCCLIENT.CODCLI
+AND PCPEDC.CODPRACA = PCPRACA.CODPRACA
+AND PCPEDC.DTCANCEL IS NULL
+AND PCPRODUT.CODFORNEC = PCFORNEC.CODFORNEC
+AND PCFORNEC.CODFORNEC = 3809
+AND PCPEDC.CODFILIAL IN ('2')
+AND PCPEDC.CONDVENDA NOT IN (4,8,10,13,20,98,99)
+
+GROUP BY  
+    PCPEDC.CODSUPERVISOR 
+    , PCSUPERV.NOME
+--    , PCPEDC.POSICAO
+--    , PCPEDC.DATA
+    , PCPEDC.CODUSUR
+    , PCPEDC.CODFILIAL
+--    , PCPRODUT.CODEPTO
+    , PCFORNEC.codfornec
+--    , PCPRODUT.CODPROD
+    , PCPEDC.CODCLI
+    , PCPEDC.CODGERENTE
+ORDER BY VLVENDA
