08edfe4a3cfb684a184f69b0bebca44c5b778a17
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2023-11-17 18:13:53 -0300

    LineBy

---
+++ Data-Analytcs/PCSYSTEM/R-800/R-8120/8120_FatMagna.sql
@@ -0,0 +1,174 @@
+select fatMagnatec.* from (
+
+select fatmagna.CODUSUR as CODUSur
+, fatmagna.NOME as NOME
+, sum(fatmagna.VLVENDa) as VLVENDA
+, sum(fatmagna.TOTPESO) as TOTPESO
+from (
+
+select  PCUSUARI.CODUSUR CODUSUR, PCUSUARI.NOME, 0 VLVENDA  , 0 TOTPESO
+from pcusuari where PCUSUARI.CODUSUR in (1139,1140,1141,1143,1157,1269,1379)
+
+Union all
+ SELECT 
+ 
+ PCNFSAID.CODUSUR  CODUSUR, 
+       PCUSUARI.NOME, 
+
+
+ nvl(sum(CASE WHEN NVL(PCMOVCOMPLE.VLSUBTOTITEM,0) <> 0 THEN  
+  DECODE(NVL(PCMOV.TIPOITEM,'N'),'C',0,NVL(PCMOVCOMPLE.VLSUBTOTITEM,0)) - 
+       (  ROUND((NVL(PCMOV.QT, 0) * 
+         DECODE(PCNFSAID.CONDVENDA,
+                 5,
+                 0,
+                 6,
+                 0,
+                 11,
+                 0,
+                 12,
+                 0,
+                 DECODE(PCMOV.CODOPER,'SB',0,nvl(pcmov.VLIPI,0)))),2)) -  
+       (  ROUND(NVL(PCMOV.QT, 0) * 
+         DECODE(PCNFSAID.CONDVENDA,
+                 5,
+                 0,
+                 6,
+                 0,
+                 11,
+                 0,
+                 12,
+                 0,
+                 DECODE(PCMOV.CODOPER,'SB',0,nvl(pcmov.ST,0))),2)) 
+ ELSE                                                
+       ROUND((((DECODE(PCMOV.CODOPER,                                           
+                       'S',                                                   
+                       (NVL(DECODE(PCNFSAID.CONDVENDA,                          
+                                   7,                                           
+                                   PCMOV.QTCONT,                                
+                                   PCMOV.QT),                                   
+                            0)),                                                
+                       'ST',                                                  
+                       (NVL(DECODE(PCNFSAID.CONDVENDA,                          
+                                   7,                                           
+                                   PCMOV.QTCONT,                                
+                                   PCMOV.QT),                                   
+                            0)),                                                
+                       'SM',                                                  
+                       (NVL(DECODE(PCNFSAID.CONDVENDA,                          
+                                   7,                                           
+                                   PCMOV.QTCONT,                                
+                                   PCMOV.QT),                                   
+                            0)),                                                
+                       0)) *                                                    
+             (NVL(DECODE(PCNFSAID.CONDVENDA,                                    
+                           7,                                                   
+                           (NVL(PUNITCONT, 0) - NVL(PCMOV.VLIPI, 0) -           
+                           (nvl(pcmov.ST,0)+NVL(PCMOVCOMPLE.VLSTTRANSFCD,0))) + NVL(PCMOV.VLFRETE, 0) +          
+                           NVL(PCMOV.VLOUTRASDESP, 0) +                         
+                           NVL(PCMOV.VLFRETE_RATEIO, 0) +                       
+                           DECODE(PCMOV.TIPOITEM,                               
+                                  'C',                                        
+                                  (SELECT NVL((SUM(M.QTCONT *                   
+                                                   NVL(M.VLOUTROS, 0)) /        
+                                          PCMOV.QT), 0) VLOUTROS                
+                                     FROM PCMOV M                               
+                                    WHERE M.NUMTRANSVENDA =                     
+                                          PCMOV.NUMTRANSVENDA                   
+                                      AND M.TIPOITEM = 'I'                    
+                                      AND CODPRODPRINC = PCMOV.CODPROD),        
+ 'I', NVL(PCMOV.VLOUTROS, 0),DECODE(NVL(PCNFSAID.SOMAREPASSEOUTRASDESPNF,'N'),'N',NVL((PCMOV.VLOUTROS), 0),'S',
+NVL((PCMOV.VLOUTROS-PCMOV.VLREPASSE), 0)))
+                           ,(NVL(PCMOV.PUNIT, 0) - NVL(PCMOV.VLIPI, 0) -         
+                           (nvl(pcmov.ST,0)+NVL(PCMOVCOMPLE.VLSTTRANSFCD,0))) + NVL(PCMOV.VLFRETE, 0) +          
+                           NVL(PCMOV.VLOUTRASDESP, 0) +                         
+                           NVL(PCMOV.VLFRETE_RATEIO, 0) +                       
+                           DECODE(PCMOV.TIPOITEM,                               
+                                  'C',                                        
+                                  (SELECT NVL((SUM(M.QTCONT *                   
+                                                   NVL(M.VLOUTROS, 0)) /        
+                                          PCMOV.QT), 0) VLOUTROS                
+                                     FROM PCMOV M                               
+                                    WHERE M.NUMTRANSVENDA =                     
+                                          PCMOV.NUMTRANSVENDA                   
+                                      AND M.TIPOITEM = 'I'                    
+                                      AND CODPRODPRINC = PCMOV.CODPROD),        
+ 'I', NVL(PCMOV.VLOUTROS, 0), DECODE(NVL(PCNFSAID.SOMAREPASSEOUTRASDESPNF,'N'),'N',NVL((PCMOV.VLOUTROS), 0),'S',
+NVL((PCMOV.VLOUTROS-PCMOV.VLREPASSE), 0)))
+                    ),0)))),                                                    
+             2) END),0) AS VLVENDA                                             
+                                                                           
+
+
+, nvl(sum(ROUND( (NVL(PCPRODUT.PESOBRUTO,PCMOV.PESOBRUTO) * NVL(PCMOV.QT, 0)),2)),0) AS TOTPESO
+
+-----------------------------------------------------------------------------------------------------
+
+
+
+  FROM PCNFSAID,
+       PCPRODUT,
+       PCMOV,
+       PCCLIENT,
+       PCUSUARI,
+       PCSUPERV,
+       PCPLPAG,
+       PCFORNEC,
+       PCATIVI, 
+       PCPRACA,
+       PCDEPTO,
+       PCSECAO,
+       PCPEDC,
+       PCGERENTE,
+       PCCIDADE,
+       PCMARCA,
+       PCROTAEXP,
+       PCMOVCOMPLE
+ WHERE PCMOV.NUMTRANSVENDA = PCNFSAID.NUMTRANSVENDA
+   AND PCMOV.DTMOV BETWEEN  TO_DATE(:DATAI, 'DD/MM/YYYY') AND 
+                                 TO_DATE(:DATAF, 'DD/MM/YYYY') 
+   AND PCMOV.CODPROD = PCPRODUT.CODPROD
+   AND PCNFSAID.CODPRACA = PCPRACA.CODPRACA(+)
+   AND PCATIVI.CODATIV(+) = PCCLIENT.CODATV1
+   AND PCMOV.CODCLI = PCCLIENT.CODCLI
+   AND PCFORNEC.CODFORNEC = PCPRODUT.CODFORNEC
+   AND  PCNFSAID.CODUSUR   = PCUSUARI.CODUSUR 
+   AND PCPRACA.ROTA = PCROTAEXP.CODROTA(+)
+   AND PCMOV.NUMTRANSITEM = PCMOVCOMPLE.NUMTRANSITEM(+)
+   AND PCPRODUT.CODMARCA = PCMARCA.CODMARCA(+)
+   AND PCPRODUT.CODMARCA = 67 -- MAGNATEC
+   AND PCCLIENT.CODCIDADE = PCCIDADE.CODCIDADE(+)
+  AND PCMOV.CODOPER <> 'SR' 
+  AND NVL(PCNFSAID.TIPOVENDA,'X') NOT IN ('SR', 'DF')
+  AND PCMOV.CODOPER <> 'SO' 
+   AND  NVL(PCNFSAID.CODSUPERVISOR,PCSUPERV.CODSUPERVISOR)   = PCSUPERV.CODSUPERVISOR
+   AND PCNFSAID.CODPLPAG = PCPLPAG.CODPLPAG
+   AND PCNFSAID.NUMPED = PCPEDC.NUMPED(+)
+   AND PCPRODUT.CODEPTO = PCDEPTO.CODEPTO(+)
+   AND PCPRODUT.CODSEC = PCSECAO.CODSEC(+)
+   AND PCNFSAID.CODGERENTE = PCGERENTE.CODGERENTE(+) 
+   AND PCNFSAID.CODFISCAL NOT IN (522, 622, 722, 532, 632, 732)
+   AND PCNFSAID.CONDVENDA NOT IN (4, 8, 10, 13, 20, 98, 99)
+   AND (PCNFSAID.DTCANCEL IS NULL)
+   AND (PCPRODUT.CODFORNEC IN ( 3395,3420,3663 )) 
+   AND PCNFSAID.DTSAIDA BETWEEN  TO_DATE(:DATAI, 'DD/MM/YYYY') AND 
+                                 TO_DATE(:DATAF, 'DD/MM/YYYY') 
+       AND PCMOV.CODFILIAL IN('2')
+       AND PCNFSAID.CODFILIAL IN('2')
+       AND PCMOV.DTMOV >= (SELECT MIN(DTMOV) FROM PCMOV)
+ 	AND PCPEDC.NUMPED >= (SELECT MIN(NUMPED) FROM PCPEDC)
+ 	AND PCPEDC.DATA >= (SELECT MIN(DATA) FROM PCPEDC)
+ 	AND PCNFSAID.DTSAIDA >= (SELECT MIN(DTSAIDA) FROM PCNFSAID)
+       --AND PCNFSAID.CODUSUR in (1139,1140,1141,1143,1157,1269,1379)
+       AND PCUSUARI.CODUSUR in (1139,1140,1141,1143,1157,1269,1379)
+       --AND PCUSUARI.CODUSUR = 1139
+       --,1140,1141,1143,1157,1269,1379)
+
+group by
+PCNFSAID.CODUSUR, 
+       PCUSUARI.NOME
+             ) fatmagna
+             group by 
+             fatmagna.CODUSUR
+, fatmagna.NOME
+ ) fatMagnatec
